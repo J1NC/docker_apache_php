5ad87024757f4de3b08110fa4288ed2299fe2f80
J1NC <womin1120@naver.com>
2019-11-14 11:49:00 +0900

    [ADD] 데이터베이스 sql 파일 추가

---
+++ database.sql
@@ -0,0 +1,18 @@
+CREATE TABLE User (
+    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(20) NOT NULL,
+    nickname VARCHAR(30) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    gender VARCHAR(10),
+    recommendCode VARCHAR(20) UNIQUE NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE Recommend (
+    recommender INT(11) NOT NULL,
+    recommendee INT(11) NOT NULL,
+    PRIMARY KEY(recommender, recommendee),
+    FOREIGN KEY (recommender) REFERENCES User(id),
+    FOREIGN KEY (recommendee) REFERENCES User(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
