f9299225f7696586dd7482f97ba853f72381464e
J1NC <womin1120@naver.com>
2019-11-15 15:22:00 +0900

    [ADD] 더미 데이터 추가 SQL 작성

--- database.sql
+++ database.sql
@@ -16,3 +16,29 @@ CREATE TABLE Recommend (
     FOREIGN KEY (recommender) REFERENCES User(id) ON DELETE CASCADE,
     FOREIGN KEY (recommendee) REFERENCES User(id) ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+INSERT INTO User(name, nickname, password, phone, email, gender, recommendCode)
+            VALUES
+            ('테스트','test','1234','01000000000', 'test1@gmail.com', 'male', '5dce28d495361'),
+            ('테스트','test','1234','01000000000', 'test2@gmail.com', 'male', '5dce2d1391f6f'),
+            ('테스트','test','1234','01000000000', 'test3@gmail.com', 'male', '5dce2d1715631'),
+            ('테스트','test','1234','01000000000', 'test4@gmail.com', 'male', '5dce2d1ddb254'),
+            ('테스트','test','1234','01000000000', 'test5@gmail.com', 'male', '5dce2d211d216'),
+            ('테스트','test','1234','01000000000', 'test6@gmail.com', 'male', '5dce2d1ae859e'),
+            ('테스트','test','1234','01000000000', 'test7@gmail.com', 'male', '5dce2d253fb15'),
+            ('테스트','test','1234','01000000000', 'test8@gmail.com', 'male', '5dce2d2adfd67'),
+            ('테스트','test','1234','01000000000', 'test9@gmail.com', 'male', '5dce28d49s261'),
+            ('테스트','test','1234','01000000000', 'test10@gmail.com', 'male', '5dce28d4fd41');
+
+INSERT INTO Recommend(recommender, recommendee)
+            VALUES
+            (1,2),
+            (1,3),
+            (1,4),
+            (1,5),
+            (2,6),
+            (2,7),
+            (2,8),
+            (2,9),
+            (2,10);
